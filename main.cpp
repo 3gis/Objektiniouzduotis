77a4a71f27432d16f062db24e75c457dbe5db94f
3gis <52011465+3gis@users.noreply.github.com>
2020-02-17 20:16:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -55,7 +55,7 @@ int naujasStudentas(int& studentusk, studentai*& M){
                 int limitas = rand() % 100 + 1;
                 for(int i = 0; i<limitas;i++){
                     masyvod++;
-                         namudarbai.push_back(rand() % 10);
+                         namudarbai.push_back(rand() % 10 + 1);
                     namudarbai.push_back(rand() % 10); // egzamino balas
                 }
             }
