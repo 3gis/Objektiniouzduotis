7f7529047cb3174ca1795687e3833e08d885f751
3gis <52011465+3gis@users.noreply.github.com>
2020-02-28 18:26:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include <sstream>
 #include <stdio.h>
 
-using std::cout;
+using std::cout; // pakeista
 using std::endl;
 using std::cin;
 using std::string;
@@ -23,24 +23,39 @@ struct studentai {
     double galutinisMed;
 };
 
-int naujasStudentas(int& studentusk, vector<studentai>& M);
+int naujasStudentas(int& studentusk, vector<studentai>& M, bool& vidurkis);
 double mediana(vector<int>M, int masyvod);
 bool lyginimas(const studentai& a, const studentai& b);
-void spausdinti(vector<studentai> studentas);
+void spausdinti(vector<studentai> studentas, bool vidurkis);
 
 int main(){
     int studentusk=-1;
+    string pasirinkimas;
+    bool vidurkis = false;
+    while(true){
+        cout << "Skaiciuoti vidurki? (jei ne, bus skaiciuojama mediana) (Y/N)";
+        cin >> pasirinkimas;
+        if(pasirinkimas=="y" || pasirinkimas=="Y"){
+            vidurkis = true;
+            cout << "Bus skaiciuojamas vidurkis..\n";
+            break;
+        }
+        else if (pasirinkimas=="n" || pasirinkimas=="N"){
+            cout << "Bus skaiciuojama mediana..\n";
+            break;
+            }
+        }
     vector<studentai> studentas;
-    naujasStudentas(studentusk, studentas);
+    naujasStudentas(studentusk, studentas,vidurkis);
     sort(studentas.begin(), studentas.end(), lyginimas);
-    spausdinti(studentas);
+    spausdinti(studentas,vidurkis);
     return 0;
 }
 
-int naujasStudentas(int& studentusk, vector<studentai>& M){
+int naujasStudentas(int& studentusk, vector<studentai>& M, bool& vidurkis){
     string pasirinkimas;
     string p;
-    int suma=0;
+    float suma=0;
     int masyvod=0;
     vector<int> namudarbai;
     while(true){
@@ -83,15 +98,18 @@ int naujasStudentas(int& studentusk, vector<studentai>& M){
                     }
                     ss >> p;
                     namudarbai.push_back(atof(p.c_str()));
-                    M[studentusk].galutinisVid=((double)(suma/(stulpeliai-3)) * 0.40 + 0.60 * atof(p.c_str()));
-                    sort(namudarbai.begin(),namudarbai.end());
-                    M[studentusk].galutinisMed=mediana(namudarbai, stulpeliai-2);
-                    //cout << endl << M.push_back(galutinisVid << endl;
+                    if(vidurkis==true)
+                        M[studentusk].galutinisVid=((double)(suma/(stulpeliai-3)) * 0.40 + 0.60 * atof(p.c_str()));
+                    else{
+                        sort(namudarbai.begin(),namudarbai.end());
+                        M[studentusk].galutinisMed=mediana(namudarbai, stulpeliai-2);
+                    }
                     suma = 0;
-                    //cout << studentusk << endl;
+                    namudarbai.clear();
+                    //cout << studentusk << endl;*/
                 }
                 cout << "Nuskaityta: " << studentusk+1 << " rezultatu \n";
-                naujasStudentas(studentusk,M);
+                naujasStudentas(studentusk,M,vidurkis);
                 return 0;
             }
             else{
@@ -181,32 +199,40 @@ int naujasStudentas(int& studentusk, vector<studentai>& M){
             suma+=namudarbai[i];
         M[studentusk].galutinisVid = (double)suma/(masyvod-1) * 0.4 + 0.6 * namudarbai.back();
         sort(namudarbai.begin(), namudarbai.end());
-        mediana(namudarbai,masyvod);
+        M[studentusk].galutinisMed = mediana(namudarbai,masyvod);
         cout << "-----------------------------------------" << endl;
-        naujasStudentas(studentusk,M);
+        namudarbai.clear();
+        naujasStudentas(studentusk,M,vidurkis);
 }
 double mediana(vector<int>M, int masyvod){
-    double ats;
-    ats = masyvod%2==0 ? (double)(((M[(masyvod/2)-2]) + (M[(masyvod/2)]))/2.0) : M[(masyvod/2)];
-    return ats;
+    return masyvod%2==0 ? (double)(((M[(masyvod/2)-2]) + (M[(masyvod/2)]))/2.0) : M[(masyvod/2)];
 }
 bool lyginimas(const studentai& a, const studentai& b){
     return a.pavarde < b.pavarde;
 }
-void spausdinti(vector<studentai> studentas){
+void spausdinti(vector<studentai> studentas, bool vidurkis){
     char buffer[80];
-    ofstream ff ("kursiokai.txt"); //pakeista
-    sprintf(buffer, "%-20s %-20s %-20s %-20s \n", "Pavarde", "Vardas", "Galutinis (Vid.)", "Galutinis (Med.)");
-    ff << buffer; //pakeista
+    ofstream ff ("kursiokai.txt");
+    if(vidurkis==true)
+        sprintf(buffer, "%-20s %-20s %-20s \n", "Pavarde", "Vardas", "Galutinis (Vid.)");
+    else
+        sprintf(buffer, "%-20s %-20s %-20s \n", "Pavarde", "Vardas", "Galutinis (Med.)");
+    ff << buffer;
     for(int i = 0; i<80;i++,ff << "-");
     ff << "\n";
-    for(auto i : studentas){
-        sprintf(buffer,"%-20s %-20s %-20.2lf %-20.2lf \n", i.pavarde.c_str(), i.vardas.c_str(), i.galutinisVid, i.galutinisMed);
-        ff << buffer;
+    if(vidurkis==true){
+        for(auto i : studentas){
+                sprintf(buffer,"%-20s %-20s %-20.2lf \n", i.pavarde.c_str(), i.vardas.c_str(), i.galutinisVid);
+            ff << buffer;
+        }
+    }
+    else{
+        for(auto i : studentas){
+                sprintf(buffer,"%-20s %-20s %-20.2lf \n", i.pavarde.c_str(), i.vardas.c_str(), i.galutinisMed);
+            ff << buffer;
+        }
     }
     cout << "Rezultatai irasyti i kursiokai.txt faila \n";
 
 
 }
-
-
