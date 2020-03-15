cc3894a2452e04ad40ea5e715c9b12b750acfaa8
3gis <52011465+3gis@users.noreply.github.com>
2020-03-15 15:55:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -35,24 +35,12 @@ int main(){
             vector <studentai> studentas;
             TestNuskaitymas("Pirmas.txt", k, studentas);
             cout << "Sarasas rikiuojamas su funkcija sort.. \n";
-             auto start = std::chrono::high_resolution_clock::now();
             sort(studentas.begin(), studentas.end(),testLyginimas);
-            auto end = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> diff = end-start;
-            cout << "Rikiavimas uztruko: " << diff.count() << "\n";
             cout << "Sarasas rusiuojamas i vargsiukus ir kietekus \n";
             TestRusiavimas(k,studentas, kietekai, vargsiukai);
             cout << "Spausdinami sarasai i atskirus failus.. \n";
-            start = std::chrono::high_resolution_clock::now();
             spausdinti(kietekai,true,"kietekai.txt");
-            end = std::chrono::high_resolution_clock::now();
-            diff = end-start;
-            cout << "Rikiavimas uztruko: " << diff.count() << "\n";
-            start = std::chrono::high_resolution_clock::now();
             spausdinti(vargsiukai,true, "vargsiukai.txt");
-            end = std::chrono::high_resolution_clock::now();
-            diff = end-start;
-            cout << "Rikiavimas uztruko: " << diff.count() << "\n";
         }
     }
     return 0;
