66923ff18875688f0485e7cbac9b319595caccd0
3gis <52011465+3gis@users.noreply.github.com>
2020-04-05 03:29:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,35 +24,48 @@ int main(){
             vector<studentai> studentas;
             naujasStudentas(studentusk, studentas,vidurkis);
             sort(studentas.begin(), studentas.end(), lyginimas);
-            spausdinti(studentas,vidurkis, "kursiokai.txt");
+            ofstream ff ("kursiokai.txt");
+            spausdinti(studentas,vidurkis, ff);
         }
         else if(pasirinkimas=="y" || pasirinkimas=="Y"){
-            int k = 5;
-            cout << "Kuriami atsitiktiniai failai... \n";
-            Generuotifailus("Pirmas.txt", k);
+            int k;
+
+
             vector <studentai> kietekai;
             vector <studentai> vargsiukai;
             vector <studentai> studentas;
-            TestNuskaitymas("Pirmas.txt", k, studentas);
-            cout << "Sarasas rikiuojamas su funkcija sort.. \n";
-             auto start = std::chrono::high_resolution_clock::now();
-            sort(studentas.begin(), studentas.end(),testLyginimas);
-            auto end = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> diff = end-start;
-            cout << "Rikiavimas uztruko: " << diff.count() << "\n";
-            cout << "Sarasas rusiuojamas i vargsiukus ir kietekus \n";
-            TestRusiavimas(k,studentas, kietekai, vargsiukai);
-            cout << "Spausdinami sarasai i atskirus failus.. \n";
-            start = std::chrono::high_resolution_clock::now();
-            spausdinti(kietekai,true,"kietekai.txt");
-            end = std::chrono::high_resolution_clock::now();
-            diff = end-start;
-            cout << "Rikiavimas uztruko: " << diff.count() << "\n";
-            start = std::chrono::high_resolution_clock::now();
-            spausdinti(vargsiukai,true, "vargsiukai.txt");
-            end = std::chrono::high_resolution_clock::now();
-            diff = end-start;
-            cout << "Rikiavimas uztruko: " << diff.count() << "\n";
+            cout << "Kuriami failai..\n ";
+            ofstream fk ("kietekai.txt");
+            ofstream fv ("vargsiukai.txt");
+            string failuPavadinimai[5]={"Pirmas.txt","Antras.txt","Trecias.txt","Ketvirtas.txt","Penktas.txt"};
+            for(k = 0; k<5;k++){
+
+                Generuotifailus(failuPavadinimai[k], k);
+                TestNuskaitymas(failuPavadinimai[k], k, studentas);
+                cout << "Sarasas rikiuojamas su funkcija sort.. \n";
+                 auto start = std::chrono::high_resolution_clock::now();
+                sort(studentas.begin(), studentas.end(),testLyginimas);
+                auto end = std::chrono::high_resolution_clock::now();
+                std::chrono::duration<double> diff = end-start;
+                cout << "=== Rikiavimas uztruko: " << diff.count() << "\n";
+                cout << "Sarasas rusiuojamas i vargsiukus ir kietekus \n";
+                TestRusiavimas(k,studentas, kietekai, vargsiukai);
+                cout << "Spausdinami sarasai i atskirus failus.. \n";
+                start = std::chrono::high_resolution_clock::now();
+                spausdinti(kietekai,true,fk);
+                end = std::chrono::high_resolution_clock::now();
+                diff = end-start;
+                cout << "=== Kieteku spausdinimas uztruko: " << diff.count() << "\n";
+                start = std::chrono::high_resolution_clock::now();
+                spausdinti(vargsiukai,true, fk);
+                end = std::chrono::high_resolution_clock::now();
+                diff = end-start;
+                cout << "=== Vargsiuku spausdinimas uztruko: " << diff.count() << "\n";
+                studentas.clear();
+                vargsiukai.clear();
+                kietekai.clear();
+                cout << "----------------------------\n";
+            }
         }
     }
     return 0;
