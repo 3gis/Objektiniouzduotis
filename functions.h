2216bc11d99a30ddba64b42ef7d0a22970c134b0
3gis <52011465+3gis@users.noreply.github.com>
2020-04-05 15:49:28 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <iomanip>
-#include <vector>
+#include <deque>
 #include <algorithm>
 #include <cmath>
 #include <string>
@@ -11,16 +11,14 @@
 #include <sstream>
 #include <stdio.h>
 #include <chrono>
-#include <deque>
 
 using std::cout;
 using std::endl;
 using std::cin;
 using std::string;
-using std::vector;
+using std::deque;
 using std::ifstream;
 using std::ofstream;
-using std::deque;
 
 class studentai {
     public:
@@ -34,7 +32,7 @@ int naujasStudentas(int& studentusk, deque<studentai>& M, bool& vidurkis);
 double mediana(deque<int>M, int masyvod);
 bool lyginimas(const studentai& a, const studentai& b);
 bool testLyginimas(const studentai& a, const studentai& b);
-void spausdinti(deque<studentai>& studentas, bool vidurkis, string pavadinimas);
+void spausdinti(deque<studentai>& studentas, bool vidurkis, ofstream& ff);
 void Generuotifailus(string a, int& b);
 void TestNuskaitymas(string pasirinkimas, int& b, deque <studentai>& M);
 void TestRusiavimas(int& k,deque <studentai>& studentas, deque<studentai>& kietekai, deque<studentai>& vargsiukai);
