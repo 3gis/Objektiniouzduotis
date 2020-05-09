d64f12de4f58ced609eae942469c97b75cea12a5
3gis <52011465+3gis@users.noreply.github.com>
2020-05-09 23:10:07 +0300

    Update functions.h

--- Source/List/functions.h
+++ Source/List/functions.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <iomanip>
-#include <list>
+#include <vector>
 #include <algorithm>
 #include <cmath>
 #include <string>
@@ -11,43 +11,45 @@
 #include <sstream>
 #include <stdio.h>
 #include <chrono>
-#include <vector>
 
-using std::vector;
 using std::cout;
 using std::endl;
 using std::cin;
 using std::string;
-using std::list;
+using std::vector;
 using std::ifstream;
 using std::ofstream;
 
-class studentai {
-private:
+class zmogus{
+protected:
     string vardas;
     string pavarde;
+public:
+    inline virtual string getVardas() const  {return vardas;}
+    inline virtual string getPavarde() const {return pavarde;}
+    inline virtual void setVardas(string x) {this-> vardas = x;}
+    inline virtual void setPavarde(string x) {this-> pavarde = x;}
+};
+class studentai : public zmogus {
+private:
     double galutinisVid;
     double galutinisMed;
 public:
     studentai& operator=(const studentai& gc1);
-    inline string getVardas() const  {return vardas;}
-    inline string getPavarde() const {return pavarde;}
     inline double getGalutinisVid() const {return galutinisVid;}
     inline double getGalutinisMed() const {return galutinisMed;}
-    inline void setVardas(string x) {this-> vardas = x;}
-    inline void setPavarde(string x) {this-> pavarde = x;}
     inline void setGalutinisVid(double x){ this->galutinisVid = x;}
     inline void setGalutinisMed(double x){ this->galutinisMed = x;}
 
 };
 ofstream& operator << (ofstream& os, const studentai& gc);
-int naujasStudentas(int& studentusk, list<studentai>& M, bool& vidurkis);
+int naujasStudentas(int& studentusk, vector<studentai>& M, bool& vidurkis);
 double mediana(vector<int>M, int masyvod);
 bool lyginimas( studentai& a, studentai& b);
-bool testLyginimas(studentai& a, studentai& b);
-void spausdinti(list<studentai>& studentas, bool vidurkis, ofstream& ff);
+bool testLyginimas( studentai& a,  studentai& b);
+void spausdinti(vector<studentai>& studentas, bool vidurkis, ofstream& ff);
 void Generuotifailus(string a, int& b);
-void TestNuskaitymas(string pasirinkimas, int& b, list <studentai>& M);
-void TestRusiavimas(int& k,list <studentai>& studentas, list<studentai>& kietekai);
+void TestNuskaitymas(string pasirinkimas, int& b, vector <studentai>& M);
+void TestRusiavimas(int& k, vector <studentai>& studentas, vector<studentai>& kietekai);
 
 #endif // FUNCTIONS_H_INCLUDED
